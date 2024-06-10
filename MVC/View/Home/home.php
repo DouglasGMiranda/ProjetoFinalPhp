e31b57505aa017adb0e87e36cec7a4fc75092d0b
Gregory-Alves <gnajr2003@gmail.com>
2024-06-10 19:28:58 -0300

    css

--- MVC/View/Home/home.php
+++ MVC/View/Home/home.php
@@ -6,8 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Document</title>
-    <link rel="stylesheet" href="../../Assets/css/Style.css">
-    <link rel="stylesheet" href="/../../Assets/css/Style.css">
+    <link rel="stylesheet" href='./bunitin.css'>
 </head>
 <body>
     <header>
--- MVC/View/Pedidos/CadastroPedidos/CadastroPedidos.php
+++ MVC/View/Pedidos/CadastroPedidos/CadastroPedidos.php
@@ -16,6 +16,7 @@ unset($_SESSION['erros']);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Pedidos</title>
+    <link rel="stylesheet" href="../../../Assets/Style.css">
 </head>
 <body>
     <header>
